0730fb9d2c3ddbef2ed2d90971035add3a992296
finnchristoffer <finn.christoffer@gmail.com>
2023-02-01 16:24:16 +0700

    Implementing Generic Network Layer in StoreHTTPClient

--- FakeStoreApp/Clients/StoreHTTPClient.swift
+++ FakeStoreApp/Clients/StoreHTTPClient.swift
@@ -40,6 +40,43 @@ struct Resource<T: Codable> {
 
 class StoreHTTPClient {
     
+    
+    func load<T: Codable>(_ resource: Resource<T>) async throws -> T {
+        
+        var request = URLRequest(url: resource.url)
+        
+        switch resource.method {
+            case .get(let queryItems):
+            var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: true)
+            components?.queryItems = queryItems
+            guard let url = components?.url else {
+                throw NetworkError.invalidURL
+            }
+            request = URLRequest(url: url)
+        case .post(let data):
+            request.httpBody = data
+        default: break
+        }
+        
+        request.allHTTPHeaderFields = resource.headers
+        request.httpMethod = resource.method.name
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        
+        let session = URLSession(configuration: configuration)
+        
+        let (data,response) = try await session.data(for: request)
+        guard let _ = response as? HTTPURLResponse
+        else {
+            throw NetworkError.invalidServerResponse
+        }
+        
+        guard let result = try? JSONDecoder().decode(T.self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        return result
+    }
     func getProductsByCategory(categoryId: Int) async throws -> [Product] {
         let (data, response) = try await URLSession.shared.data(from: URL.productsByCategory(categoryId))
         guard let httpsResponse = response as? HTTPURLResponse,
